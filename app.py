249ffa1cbed44a49b00669b1072eaeed356c7f33
anela <anela89@gmail.com>
2022-10-31 12:22:14 -0700

    Created route for start of a survey. Updated template to dynamically show title and instructions.

--- app.py
+++ app.py
@@ -7,3 +7,11 @@ app.config['SECRET_KEY'] = "never-tell!"
 app.config['DEBUG_TB_INTERCEPT_REDIRECTS'] = False
 
 debug = DebugToolbarExtension(app)
+
+responses = []
+
+@app.get('/')
+def index():
+    """Display the start page of a survey."""
+
+    return render_template("survey_start.html", survey=survey)
