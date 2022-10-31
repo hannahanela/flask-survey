b0e610bc5c1445113c6e426a07ff12b32300161d
anela <anela89@gmail.com>
2022-10-31 13:49:21 -0700

    Added route to dynamically display a survey question. Refactored question template to dynamically display choices for a question.

--- app.py
+++ app.py
@@ -9,9 +9,27 @@ app.config['DEBUG_TB_INTERCEPT_REDIRECTS'] = False
 debug = DebugToolbarExtension(app)
 
 responses = []
+question_num = 0
 
 @app.get('/')
 def index():
     """Display the start page of a survey."""
 
     return render_template("survey_start.html", survey=survey)
+
+
+@app.get('/questions/<int:num>')
+def display_question(num):
+    """Display a survey question."""
+    question = survey.questions[num]
+
+    return render_template("question.html", question=question)
+
+@app.post('/answer')
+def store_answer():
+    """Store answer for a survey question."""
+    answer = request.form["answer"]
+    responses.append(answer)
+    question_num += 1
+
+    return redirect(f"/questions/{question_num}")
